3054f6ad27eb2b9b74e6d800e216b6577ab704a6
Amit Ishairzay <amit.ishairzay@gmail.com>
2016-04-24 13:05:29 -0400

    Added before so that user db is wiped before reach test run.

--- src/test/userTest.js
+++ src/test/userTest.js
@@ -2,8 +2,15 @@ var should = require('chai').should();
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var api = supertest('http://localhost:8080');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/CollaborativeMusicPlayer');
+var user = require("../lib/schemas/userSchema");
+
 
 describe('User', function() {
+  before(function(done) {
+    user.remove({}, done);
+  });
   it('should return 200 and create a new user', function(done) {
     api.post('/users')
       .send({
