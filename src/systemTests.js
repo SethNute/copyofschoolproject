3e46b4d0e48c87c224413f57dadd18fe3c48a930
Amit Ishairzay <amit.ishairzay@gmail.com>
2016-04-25 15:15:23 -0400

    Start making log out test.

--- src/systemTests.js
+++ src/systemTests.js
@@ -1,4 +1,7 @@
 require('./app.js');
+var mongoose = require('mongoose');
+var user = require("./lib/schemas/userSchema");
+user.remove({}, function() {
 
 var webdriverio = require('webdriverio');
 var options = {
@@ -9,14 +12,30 @@ var options = {
  
 console.log('Running system tests.');
 
+var passed = true;
 
+// Tests log out functionality
 webdriverio
     .remote(options)
     .init()
     .url('http://localhost:8080')
     .getTitle().then(function(title) {
-        console.log('Title was: ' + title);
+        if(title !== 'Collaborative Music Player') {
+            passed = false;
+        }
     })
+    .setValue('#register-email-input', 'a@a.com')
+    .setValue('#register-username-input', 'a')
+    .setValue('#register-password-input', 'a')
+    .click('#register-button')
+    .click('#logout-button')
     .end(function() {
+        if(passed) {
+            console.log('Passed log in test');
+        } else {
+            console.log('Failed log in test');
+        }
         process.exit();
     });
+
+});
